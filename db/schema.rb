98387f674ecf280c5f45b7d5dd810ca108174943
Rusty Moret <rmoret@covermymeds.com>
2020-03-14 13:57:46 -0400

    Updates to log api

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_13_135558) do
+ActiveRecord::Schema.define(version: 2020_03_14_172555) do
 
   create_table "day_tags", force: :cascade do |t|
     t.integer "day_id", null: false
